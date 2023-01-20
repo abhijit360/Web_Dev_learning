724c5faec74b28ae60ddd8d798f2c27d5465c2e4
Abhijit <kamathabhijit2002@gmail.com>
2023-01-20 10:10:23 -0500

    first commit

---
+++ DOM/script.js
@@ -0,0 +1,7 @@
+movies = ['The Shawshank Redemption (1994)','The Godfather (1972)',
+'The Godfather: Part II (1974)', 'The Dark Knight (2008)','Forrest Gump (1994)',
+'Saving Private Ryan (1998)','Pulp Fiction (1994)']
+
+let main = document.querySelector('.main');
+
+console.log(main.getAttribute("title")); // null as it does not exist
